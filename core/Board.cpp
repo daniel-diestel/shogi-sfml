f84f1d63ad637772570ac46d5cdfa87ffabf6d7f
KeepGoing <diestel.daniel@gmail.com>
2025-12-26 14:40:03 +0100

    Added Board Class

---
+++ core/Board.cpp
@@ -0,0 +1,32 @@
+#include "Board.hpp"
+
+Board::Board() {
+    for (std::array<Piece*, 9>& row : m_board) {
+        row.fill(nullptr);
+    }
+}
+
+bool Board::isInside(int x, int y) const {
+    return x >= 0 && x < 9 && y >= 0 && y < 9;
+}
+
+Piece* Board::getPiece(int x, int y) const {
+    if(!isInside(x, y)) {
+        return nullptr;
+    }
+    return m_board[y][x];
+}
+
+void Board::placePiece(int x, int y, Piece* piece) {
+    if (!isInside(x, y)) {
+        return;
+    }
+    m_board[y][x] = piece;
+}
+
+void Board::removePiece(int x, int y) {
+    if (!isInside(x, y)) {
+        return;
+    }
+    m_board[y][x] = nullptr;
+}
---
+++ core/include/core/Board.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+#include "Piece.hpp"
+
+class Board {
+public:
+    Board();
+
+    Piece* getPiece(int x, int y) const;
+    void placePiece(int x, int y, Piece* piece);
+    void removePiece(int x, int y);
+
+private:
+    // Board coordinates:
+    // x = column [0..8], left → right
+    // y = row    [0..8], top  → bottom
+    std::array<std::array<Piece*, 9>, 9> m_board;
+
+    bool isInside(int x, int y) const;
+};
